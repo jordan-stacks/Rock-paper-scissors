61c0a41acea13f8903c113c80240fdad08094bf8
jordan-stacks <61798889+jordan-stacks@users.noreply.github.com>
2020-03-04 17:27:49 +0000

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,3 +1,5 @@
+import random
+
 #tracking wins
 user_wins = 0
 comp_wins = 0
@@ -5,52 +7,36 @@ comp_wins = 0
 lose = ('Haha, you got beat by 50 lines of code')
 win = ('You won this round')
 
-user_choice = input('Choose rock, paper or scissors: \n')
+user_input = input('Choose rock, paper or scissors: \n')
 
-if user_choice in ['Rock', 'rock', 'R', 'r']:
-    user_choice = r
-
-if user_choice in ['Paper', 'paper', 'P', 'p']:
-    user_choice = p
-
-if user_choice in ['Scissors', 'scissors', 'S', 's']:
-    user_choice = s
-
+if user_input in ['Rock', 'rock', 'R', 'r']:
+    user_choice = 'r'
+elif user_input in ['Paper', 'paper', 'P', 'p']:
+    user_choice = 'p'
+elif user_input in ['Scissors', 'scissors', 'S', 's']:
+    user_choice = 's'
 else:
     print('Thats not rock paper or scissors')
+    exit()
 
-import random
 comp_choice = random.randint(1,3)
+comp_choice = 3
 
 if comp_choice == 1:
-    comp_choice = r
-
+    comp_choice = 'r'
 elif comp_choice == 2:
-    comp_choice = p
-
+    comp_choice = 'p'
 else:
-    comp_choice = s
+    comp_choice = 's'
 
 #comparing user to comp choice
 if user_choice == comp_choice:
     print('Issa draw')
-
-if user_choice == r and comp_choice == p:
-    print(lose)
-
-if user_choice == p and comp_choice == s:
-    print(lose)
-
-if user_choice == s and comp_choice == r:
-    print(lose)
-
-if user_choice == r and comp_choice == s:
-    print(win)
-
-if user_choice == s and comp_choice == p:
-    print(win)
-
-if user_choice == p and comp_choice == r:
+elif (user_choice == 'r' and comp_choice == 'p') \
+    or (user_choice == 'p' and comp_choice == 's') \
+    or (user_choice == 's' and comp_choice == 'r'):
+        print(lose)
+else:
     print(win)
 
 print('Your wins', user_wins)
